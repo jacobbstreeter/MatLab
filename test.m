f7aaf5da79208e9997204890fc82e25e5b1b48bb
Jacob Streeter <js2368@byu.edu>
2022-01-05 19:54:39 -0700

    continue on lab #1 find out degree

--- test.m
+++ test.m
@@ -2,10 +2,18 @@ clear
 clc
 clear all
 
-x = sin(2)
-N = input('Enter a value for N - ');
-fprintf(' N =%g \n',500)
-fprintf(' x =%1.12g \n',pi)
-fprintf(' x =%1.10e \n',pi)
-fprintf(' x =%6.2f \n',pi)
-fprintf(' x =%12.8f y =%12.8f \n',5,exp(5))
+% x = sin(2)
+% N = input('Enter a value for N - ');
+% fprintf(' N =%g \n',500)
+% fprintf(' x =%1.12g \n',pi)
+% fprintf(' x =%1.10e \n',pi)
+% fprintf(' x =%6.2f \n',pi)
+% fprintf(' x =%12.8f y =%12.8f \n',5,exp(5))
+
+%a)
+d = 10000;
+v = 750;
+g = 9.8;
+
+x = (asin((d*g)/v^2))/2
+
