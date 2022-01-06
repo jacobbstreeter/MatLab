0c8e1654d4229113f90efa039c548f05aa12b4ca
Jacob Streeter <js2368@byu.edu>
2022-01-05 19:42:17 -0700

    Added CH 2 stuff

--- test.m
+++ test.m
@@ -3,3 +3,9 @@ clc
 clear all
 
 x = sin(2)
+N = input('Enter a value for N - ');
+fprintf(' N =%g \n',500)
+fprintf(' x =%1.12g \n',pi)
+fprintf(' x =%1.10e \n',pi)
+fprintf(' x =%6.2f \n',pi)
+fprintf(' x =%12.8f y =%12.8f \n',5,exp(5))
