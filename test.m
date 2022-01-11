c8f4e1c733ff40b9141cee548a771857f5af12f1
jacobbstreeter <94476842+jacobbstreeter@users.noreply.github.com>
2022-01-11 11:48:22 -0700

    further into CH 4

--- test.m
+++ test.m
@@ -62,3 +62,19 @@ for n=1:N % start of the loop
 s = s + 1/n^2; % add 1/n^2 to s each iteration
 end % end of the loop
 fprintf(' Sum = %g \n',s) % print the answer
+
+%% 
+
+clear; close all;
+P=1; % set the first term in the product
+N=20; % set the upper limit of the product
+for n=2:N % start the loop at n=2 because we already loaded n=1
+P=P*n; % multiply by n each time, put the answer back into P
+end % end of the loop
+fprintf(' N! = %g \n',P) % print the answer
+
+factorial(20)
+
+%% 
+
+
