12dd0f3c3dbb173d224e373da115aa3e39dd8a83
jacobbstreeter <94476842+jacobbstreeter@users.noreply.github.com>
2022-01-11 12:00:40 -0700

    Finished Ch 4 in intro

--- test.m
+++ test.m
@@ -33,7 +33,7 @@ A = [1  2  3  4
      5  6  7  8
      9 10 11 12
      13 14 15 16]
- %% 
+ %% 3.1 Debugging
  
 clear; % clear all variables from memory
 close all; % close any figure windows
@@ -52,7 +52,7 @@ fprintf(' 3 is not a factor of %g \n', n);
 end
 end
 
-%% 
+%% 4.1 Loops and Logic
 
 clear; close all;
 % set s to zero so that 1/n^2 can be repeatedly added to it
@@ -63,7 +63,7 @@ s = s + 1/n^2; % add 1/n^2 to s each iteration
 end % end of the loop
 fprintf(' Sum = %g \n',s) % print the answer
 
-%% 
+%% 4.2 Loops
 
 clear; close all;
 P=1; % set the first term in the product
@@ -75,6 +75,65 @@ fprintf(' N! = %g \n',P) % print the answer
 
 factorial(20)
 
+%% 4.3 Loops in matrix
+
+clear; close all;
+a(1)=1; % put the first element into the array
+N=19; % the first one is loaded, so let's load 19 more
+for n=1:N % start the loop
+a(n+1)=(2*n-1)/(2*n+1)*a(n); % the recursion relation
+end
+disp(a) % display the resulting array of values
+
+%% 4.4 Logic (if/than)
+
+clear; close all;
+a=1;
+b=3;
+if a>0
+    c=1 % If a is positive set c to 1
+else
+    c=0 %if a is 0 or negative, set c to zero
+end
+
+% if either a or b is non-negative, add them to obtain c;
+% otherwise multiply a and b to obtain c
+if a>=0 | b>=0 % either non-negative
+    c=a+b
+else
+    c=a*b % otherwise multiply them to obtain c
+end
+
+%% 4.5 While loop
+
+clear; close all;
+term=1 % load the first term in the sum, 1/1^2=1
+s=term; % load s with this first term
+% start of the loop - set a counter n to one
+n=1;
+while term > 1e-10 % loop until term drops below 1e-10
+n=n+1; % add 1 to n so that it will count: 2,3,4,5,...
+term=1/n^2; % calculate the next term to add
+s=s+term; % add 1/n^2 to s until the condition is met
+end % end of the loop
+fprintf(' Sum = %g \n',s)
+
 %% 
 
-
+clear; close all;
+term=1; % load the first term in the sum, 1/1^2=1
+s=term; % load s with this first term
+% start of the loop - set a counter n to one
+n=1;
+while term > 1e-100 % set a ridiculously small term.
+% Don't really do this, as you
+% only have 15 digits of precision.
+n=n+1; % add 1 to n so that it will count: 2,3,4,5,...
+term=1/n^2;
+s=s+term;
+if (n > 1000) % Break stop if it is taking too long
+disp('This is taking too long. I''m out of here...')
+break
+end
+end % end of the loop
+fprintf(' Sum = %g \n',s)
