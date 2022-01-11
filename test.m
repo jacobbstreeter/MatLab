000aa2c684af14d1bd7a84ac092615a04aa4cdbb
jacobbstreeter <94476842+jacobbstreeter@users.noreply.github.com>
2022-01-11 11:40:32 -0700

    moved to CH 4

--- test.m
+++ test.m
@@ -33,6 +33,7 @@ A = [1  2  3  4
      5  6  7  8
      9 10 11 12
      13 14 15 16]
+ %% 
  
 clear; % clear all variables from memory
 close all; % close any figure windows
@@ -50,4 +51,14 @@ else
 fprintf(' 3 is not a factor of %g \n', n);
 end
 end
-      
+
+%% 
+
+clear; close all;
+% set s to zero so that 1/n^2 can be repeatedly added to it
+s=0;
+N=10000; % set the upper limit of the sum
+for n=1:N % start of the loop
+s = s + 1/n^2; % add 1/n^2 to s each iteration
+end % end of the loop
+fprintf(' Sum = %g \n',s) % print the answer
